2e88c6d5501c6692a176aa4ebe34f7dff1455712
nolan908 <123701723+nolan908@users.noreply.github.com>
2026-05-26 18:04:13 -0400

    Allow deployed API requests

--- src/main/java/com/cookbook/cookbook/SecurityConfig.java
+++ src/main/java/com/cookbook/cookbook/SecurityConfig.java
@@ -28,8 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
